b4b53729b114cfd5640b1a8aa4b02ae53cae75e4
Andressa Atelli <andressaatelli.programador@gmail.com>
2022-06-28 21:35:50 -0300

    token

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("v1/users")]
+    public class UserController : Controller
+    {
+        
+    }
+}
